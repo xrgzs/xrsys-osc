f7068cc8c9762e45ff571a48e67fbd2b7938cf53
xrgzs <dalizxr@qq.com>
2024-04-03 07:00:17 +0800

    更新 UWP 运行库

--- osc/runtime/Extension/install.bat
+++ osc/runtime/Extension/install.bat
@@ -2,13 +2,13 @@ cd /d "%~dp0"
 for %%a in (
 Microsoft.VCLibs.140.00_14.0.33519.0_x64__8wekyb3d8bbwe.Appx
 Microsoft.VCLibs.140.00_14.0.33519.0_x86__8wekyb3d8bbwe.Appx
-Microsoft.AV1VideoExtension_1.1.62361.0_neutral_~_8wekyb3d8bbwe.AppxBundle
-Microsoft.HEIFImageExtension_1.0.63001.0_neutral_~_8wekyb3d8bbwe.AppxBundle
+Microsoft.AV1VideoExtension_1.2.661.0_neutral_~_8wekyb3d8bbwe.AppxBundle
+Microsoft.HEIFImageExtension_1.1.861.0_neutral_~_8wekyb3d8bbwe.AppxBundle
 Microsoft.MPEG2VideoExtension_1.0.61931.0_neutral_~_8wekyb3d8bbwe.AppxBundle
-Microsoft.RawImageExtension_2.2.172.0_neutral_~_8wekyb3d8bbwe.AppxBundle
-Microsoft.VP9VideoExtensions_1.1.41.0_neutral_~_8wekyb3d8bbwe.AppxBundle
-Microsoft.WebMediaExtensions_1.0.62931.0_neutral_~_8wekyb3d8bbwe.AppxBundle
-Microsoft.WebpImageExtension_1.1.171.0_neutral_~_8wekyb3d8bbwe.AppxBundle
+Microsoft.RawImageExtension_2.3.611.0_neutral_~_8wekyb3d8bbwe.AppxBundle
+Microsoft.VP9VideoExtensions_1.1.451.0_neutral_~_8wekyb3d8bbwe.AppxBundle
+Microsoft.WebMediaExtensions_1.1.663.0_neutral_~_8wekyb3d8bbwe.AppxBundle
+Microsoft.WebpImageExtension_1.1.861.0_neutral_~_8wekyb3d8bbwe.AppxBundle
 ) do (
 call Powershell -Command Add-AppxProvisionedPackage -SkipLicense -Online -PackagePath "%%~fa"
 )
