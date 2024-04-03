80feda5d61ee088e44fdca5b6904e3d2e2492674
xrgzs <dalizxr@qq.com>
2024-04-04 05:23:02 +0800

    更换避免程序卡住的策略

--- osc/optimize/yrxitong/yr.bat
+++ osc/optimize/yrxitong/yr.bat
@@ -4,8 +4,7 @@ cd /d "%~dp0"
 ver | find /i "5.0." && exit
 ver | find /i "5.1." && exit
 echo [OSC]正在使用渔具优化系统...>"%systemdrive%\Windows\Setup\wallname.txt"
-start "" "yr.exe" /OPTini=[Optimization]OSC.ini
-timeout -t 30 2>nul || ping 127.0.0.1 -n 30 >nul
+start "" /wait "%PECMD%" EXEC -wait -timeout:30000 yr.exe /OPTini=[Optimization]OSC.ini
 
 if exist "%SystemDrive%\Windows\Setup\xroknoad.txt" (
 	exit
