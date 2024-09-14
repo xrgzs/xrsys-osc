4f18b1c9d62c9c47d2fd1ce71b10d9e23703002c
xrgzs <xiaoran@xrgzs.top>
2024-09-14 14:07:33 +0800

    更新 播放器运行库 34.0.0.321

--- build.bat
+++ build.bat
@@ -53,7 +53,7 @@ curl.exe -sSL "https://api.xrgzs.top/lanzou/?type=down&url=https://xrgzs.lanzouv
 rem https://github.com/stdin82/htfx/releases v0.0.3
 curl.exe -sSL "https://api.xrgzs.top/lanzou/?type=down&url=https://xrgzs.lanzouv.com/irdVI27gi2pg" -o osc\runtime\DirectX_Redist_Repack_x86_x64_Final.exe || exit
 rem Special purpose, not describable v34.0.0.317
-curl.exe -sSL "https://api.xrgzs.top/lanzou/?type=down&url=https://xrgzs.lanzouv.com/iSrMk27gi3gd" -o osc\runtime\flash.exe || exit
+curl.exe -sSL "https://api.xrgzs.top/lanzou/?type=down&url=https://xrgzs.lanzouv.com/ihcxj29z4hbe" -o osc\runtime\flash.exe || exit
 @rem rem MSVC Runtime repacked by dreamcast2.ys168.com
 @rem rem curl.exe -sSL xxxxxxxxxx" -o osc\runtime\MSVBCRT.AIO.exe || exit
 rem MSVC Runtime repacked by Xiaoran Studio using NSIS v2024.8.14.0
