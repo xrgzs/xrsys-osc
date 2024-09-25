850bc23f1d60c4545b9658c2dfe770e0c5e6dd68
xrgzs <xiaoran@xrgzs.top>
2024-09-26 00:33:25 +0800

    增加 多个蓝奏API避免构建时出错

--- build.bat
+++ build.bat
@@ -1,7 +1,6 @@
 chcp 936
 PUSHD %~dp0
 
-
 :check
 echo version: %GITHUB_WORKFLOW_VERSION%
 rem lookup nsis location
@@ -14,25 +13,25 @@ if not exist "C:\Program Files (x86)\NSIS\makensis.exe" (
 if exist osc\xrsoft.exe goto build
 
 rem https://github.com/stdin82/htfx/releases v0.0.3
-curl.exe -sSL "https://api.xrgzs.top/lanzou/?type=down&url=https://xrgzs.lanzouv.com/irdVI27gi2pg" -o osc\runtime\DirectX_Redist_Repack_x86_x64_Final.exe || exit
+call :downloadlanzou "https://xrgzs.lanzouv.com/irdVI27gi2pg" osc\runtime\DirectX_Redist_Repack_x86_x64_Final.exe
 rem Special purpose, not describable v34.0.0.317
-curl.exe -sSL "https://api.xrgzs.top/lanzou/?type=down&url=https://xrgzs.lanzouv.com/ihcxj29z4hbe" -o osc\runtime\flash.exe || exit
+call :downloadlanzou "https://xrgzs.lanzouv.com/ihcxj29z4hbe" osc\runtime\flash.exe
 @rem rem MSVC Runtime repacked by dreamcast2.ys168.com
 @rem rem curl.exe -sSL xxxxxxxxxx" -o osc\runtime\MSVBCRT.AIO.exe || exit
 rem MSVC Runtime repacked by Xiaoran Studio using NSIS v2024.8.14.0
-curl.exe -sSL "https://api.xrgzs.top/lanzou/?type=down&url=https://xrgzs.lanzouv.com/iWN6I27gi6ch" -o osc\runtime\MSVCRedist.AIO.exe || exit
+call :downloadlanzou "https://xrgzs.lanzouv.com/iWN6I27gi6ch" osc\runtime\MSVCRedist.AIO.exe
 
 rem https://github.com/abbodi1406/KMS_VL_ALL_AIO v52 modified server address
-curl.exe -sSL "https://api.xrgzs.top/lanzou/?type=down&url=https://xrgzs.lanzouv.com/iEOKz27gibqb" -o osc\xrkms\KMS_VL_ALL_AIO.cmd || exit
+call :downloadlanzou "https://xrgzs.lanzouv.com/iEOKz27gibqb" osc\xrkms\KMS_VL_ALL_AIO.cmd
 rem https://github.com/zbezj/HEU_KMS_Activator v42.2.0
-curl.exe -sSL "https://api.xrgzs.top/lanzou/?type=down&url=https://xrgzs.lanzouv.com/i8NFT2a8fagh" -o osc\xrkms\kms.exe || exit
+call :downloadlanzou "https://xrgzs.lanzouv.com/i8NFT2a8fagh" osc\xrkms\kms.exe
 
 rem oscol offline
-curl.exe -sSL http://url.xrgzs.top/osconline -o osc\oscoffline.bat || exit
-curl.exe -sSL http://url.xrgzs.top/oscsoft -o osc\oscsoftof.txt || exit
+curl.exe -SL --connect-timeout 5 http://url.xrgzs.top/osconline -o osc\oscoffline.bat || exit
+curl.exe -SL --connect-timeout 5 http://url.xrgzs.top/oscsoft -o osc\oscsoftof.txt || exit
 
 rem good thing
-curl.exe -sSL "https://api.xrgzs.top/lanzou/?type=down&url=https://xrgzs.lanzouv.com/ixdbP27giisf" -o osc\xrsoft.exe || exit
+call :downloadlanzou "https://xrgzs.lanzouv.com/ixdbP27giisf" osc\xrsoft.exe
 
 :build
 rem use github env version first
@@ -42,3 +41,10 @@ if not defined GITHUB_WORKFLOW_VERSION (
 >osc\apifiles\Version.txt echo %GITHUB_WORKFLOW_VERSION%
 "C:\Program Files (x86)\NSIS\makensis.exe" /V4 /DCUSTOM_VERSION=%GITHUB_WORKFLOW_VERSION% "osc.nsi" || exit
 exit
+
+:downloadlanzou <shareurl> <outpath>
+curl.exe -SL --connect-timeout 3 "https://api.xrgzs.top/lanzou/?type=down&url=%~1" -o "%~2" || (
+    @echo api.xrgzs.top failed, try another...
+    curl.exe -SL --connect-timeout 3 "https://lz.qaiu.top/parser?url=%~1" -o "%~2" || exit
+)
+goto :eof
