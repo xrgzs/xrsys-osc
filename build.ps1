d4b44cc8a2efc6490e8ca3554bbdb59ca4f86291
xrgzs <xiaoran@xrgzs.top>
2024-12-14 00:05:28 +0800

    更新 播放器运行库 34.0.0.323

--- build.ps1
+++ build.ps1
@@ -99,7 +99,7 @@ if (Test-Path 'osc\xrsoft.exe') {
 else {
     # 下载所需文件
     Get-LanzouFile -Uri "https://xrgzs.lanzouv.com/irdVI27gi2pg" -OutFile "osc\runtime\DirectX_Redist_Repack_x86_x64_Final.exe"
-    Get-LanzouFile -Uri "https://xrgzs.lanzouv.com/ihcxj29z4hbe" -OutFile "osc\runtime\flash.exe"
+    Get-LanzouFile -Uri "https://xrgzs.lanzouv.com/it4vE2i2cq3e" -OutFile "osc\runtime\flash.exe"
     Get-LanzouFile -Uri "https://xrgzs.lanzouv.com/iHwLb2eee7gd" -OutFile "osc\runtime\MSVCRedist.AIO.exe"
     Get-LanzouFile -Uri "https://xrgzs.lanzouv.com/idHOf2bfs3te" -OutFile "osc\xrkms\KMS_VL_ALL_AIO.cmd"
     Get-LanzouFile -Uri "https://xrgzs.lanzouv.com/iWI1c2c8sjqb" -OutFile "osc\xrkms\kms.exe"
