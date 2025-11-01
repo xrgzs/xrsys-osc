dd082ab7cac5895fa4229e042a7a45277d83a0f8
MadDogOwner <xiaoran@xrgzs.top>
2025-11-02 00:43:46 +0800

    移除 DX9、AFP组件
    Signed-off-by: MadDogOwner <xiaoran@xrgzs.top>

--- build.ps1
+++ build.ps1
@@ -99,8 +99,6 @@ if (Test-Path 'osc\xrsoft.exe') {
 }
 else {
     # 下载所需文件
-    Get-LanzouFile -Uri "https://xrgzs.lanzouv.com/irdVI27gi2pg" -OutFile "osc\runtime\DirectX_Redist_Repack_x86_x64_Final.exe"
-    Get-LanzouFile -Uri "https://xrgzs.lanzouv.com/ivgNv2yiocpg" -OutFile "osc\runtime\flash.exe"
     Get-LanzouFile -Uri "https://xrgzs.lanzouv.com/idHOf2bfs3te" -OutFile "osc\xrkms\KMS_VL_ALL_AIO.cmd"
     Get-LanzouFile -Uri "https://xrgzs.lanzoum.com/iIse82yindri" -OutFile "osc\xrkms\kms.exe"
     Get-LanzouFile -Uri "https://xrgzs.lanzouv.com/iqnTr2wxjufc" -OutFile "osc\xrsoft.exe"
