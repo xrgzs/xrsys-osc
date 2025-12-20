224f05d1c19663b00c2ec13d322f0c86cf764b13
MadDogOwner <xiaoran@xrgzs.top>
2025-12-20 11:32:18 +0800

    优化 构建-依赖文件下载
    Signed-off-by: MadDogOwner <xiaoran@xrgzs.top>

--- build.ps1
+++ build.ps1
@@ -5,6 +5,22 @@ $ErrorActionPreference = 'Stop'
 Set-Location $PSScriptRoot
 
 # 下载文件
+function Invoke-RobustRequest {
+    param($Url, $OutFile)
+    for ($retry = 1; $retry -le 3; $retry++) {
+        try {
+            Invoke-WebRequest -Uri $Url -OutFile $OutFile -ConnectionTimeoutSeconds 5 -AllowInsecureRedirect
+            return
+        }
+        catch {
+            if ($retry -eq 3) {
+                throw
+            }
+            Write-Host "Retry $retry failed for $Url, trying again ($retry / 3)..."
+        }
+    }
+}
+    
 function Get-LanzouFile {
     param(
         [Parameter(Mandatory = $true, Position = 0)]
@@ -15,15 +31,16 @@ function Get-LanzouFile {
         [string]
         $OutFile
     )
+    
     Write-Host "Downloading $Uri to $OutFile..."
     try {
         Write-Host "Using api.xrgzs.top to parse link..."
-        Invoke-WebRequest -Uri "https://api.xrgzs.top/sdlp/lanzou/?type=down&url=$Uri" -OutFile $OutFile -ConnectionTimeoutSeconds 5 -AllowInsecureRedirect
+        Invoke-RobustRequest -Url "https://api.xrgzs.top/sdlp/lanzou/?type=down&url=$Uri" -OutFile $OutFile
     }
     catch {
         try {
             Write-Host "Using lz.qaiu.top to parse link..."
-            Invoke-WebRequest -Uri "https://lz.qaiu.top/parser?url=$Uri" -OutFile $OutFile -ConnectionTimeoutSeconds 5 -AllowInsecureRedirect
+            Invoke-RobustRequest -Url "https://lz.qaiu.top/parser?url=$Uri" -OutFile $OutFile
         }
         catch {
             Write-Error "Failed to download $Uri. ($_)"
@@ -45,11 +62,11 @@ else {
     Get-LanzouFile -Uri "https://xrgzs.lanzouv.com/idHOf2bfs3te" -OutFile "osc\xrkms\KMS_VL_ALL_AIO.cmd"
     Get-LanzouFile -Uri "https://xrgzs.lanzoum.com/itYHu3avdgcb" -OutFile "osc\xrkms\kms.exe"
     Get-LanzouFile -Uri "https://xrgzs.lanzouv.com/iqnTr2wxjufc" -OutFile "osc\xrsoft.exe"
-    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/refs/heads/master/MAS/Separate-Files-Version/Activators/TSforge_Activation.cmd" -OutFile "osc\xrkms\TSforge_Activation.cmd"
+    Invoke-RobustRequest -Uri "https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/refs/heads/master/MAS/Separate-Files-Version/Activators/TSforge_Activation.cmd" -OutFile "osc\xrkms\TSforge_Activation.cmd"
 
     # 下载其他文件
-    Invoke-WebRequest -Uri "https://url.xrgzs.top/osconline" -OutFile "osc\oscoffline.bat" -ErrorAction Stop
-    Invoke-WebRequest -Uri "https://url.xrgzs.top/oscsoft" -OutFile "osc\oscsoftof.txt" -ErrorAction Stop
+    Invoke-RobustRequest -Uri "https://url.xrgzs.top/osconline" -OutFile "osc\oscoffline.bat" -ErrorAction Stop
+    Invoke-RobustRequest -Uri "https://url.xrgzs.top/oscsoft" -OutFile "osc\oscsoftof.txt" -ErrorAction Stop
 }
 
 # 构建
